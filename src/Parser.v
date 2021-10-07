493bff657f5fca40556db92c0f01c9fdea37c828
Arthur Correnson <arthur.correnson@gmail.com>
2021-10-07 13:36:17 +0200

    add a parser

---
+++ src/Parser.v
@@ -0,0 +1,54 @@
+Require Import String Ascii List.
+Import ListNotations.
+
+Definition bind {A B} (m : option A) (f : A -> option B) :=
+  match m with
+  | None => None
+  | Some x => f x
+  end.
+
+Inductive AST :=
+  | Incr
+  | Decr
+  | Left
+  | Right
+  | In
+  | Out
+  | Loop : list AST -> AST.
+
+
+Fixpoint parse_aux (n : nat) (s : string) (b : bool) {struct n} : option (list AST * string) :=
+  match n with
+  | 0 => None
+  | S n =>
+    match s%string with
+    | String "+"%char next =>
+      bind (parse_aux n next b) (fun '(p, next) => Some (Incr :: p, next))
+    | String "-"%char next =>
+      bind (parse_aux n next b) (fun '(p, next) => Some (Decr :: p, next))
+    | String ">"%char next =>
+      bind (parse_aux n next b) (fun '(p, next) => Some (Right :: p, next))
+    | String "<"%char next =>
+      bind (parse_aux n next b) (fun '(p, next) => Some (Left :: p, next))
+    | String "["%char next =>
+      bind (parse_aux n next true) (fun '(p1, next1) =>
+        bind (parse_aux n next1 b) (fun '(p2, next2) =>
+          Some ((Loop p1) :: p2, next2)
+        )
+      )
+    | String "]"%char next =>
+      if b then Some (nil, next) else None
+    | String _ next =>
+      bind (parse_aux n next b) (fun '(p, next) => Some (p, next))
+    | EmptyString =>
+      if b then None else Some (nil, EmptyString)
+    end
+  end.
+
+Definition parse (s : string) : option (list AST) :=
+  match parse_aux (String.length s) s false with
+  | Some (r, ""%string) => Some r
+  | _ => None
+  end.
+
+Compute (parse "[++[><<]]"%string).
